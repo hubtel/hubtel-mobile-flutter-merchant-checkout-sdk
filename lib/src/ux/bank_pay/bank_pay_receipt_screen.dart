c9f6727b28cc4360217a582e3c7a4ec3330fb5aa
Maampeaw <markamoah97@gmail.com>
2025-08-22 12:15:05 +0000

    update pubspec

--- lib/src/ux/bank_pay/bank_pay_receipt_screen.dart
+++ lib/src/ux/bank_pay/bank_pay_receipt_screen.dart
@@ -1,7 +1,8 @@
 import 'dart:developer';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_html_to_pdf2/flutter_html_to_pdf.dart';
+import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
+
 
 
 // import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
